069e26fed19d09de183e131072947907f76c1d78
uoojin1 <uoojin@hotmail.com>
2017-11-12 20:33:02 -0500

    learning about publish/subscription

--- short-lnk/imports/ui/Link.js
+++ short-lnk/imports/ui/Link.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
+import { Links } from '../api/links';
+import LinksList from '../ui/LinksList';
 
 export default class Link extends React.Component {
     onLogout() {
         Accounts.logout();
+    }
+    onSubmit(e) {
+        const url = this.refs.url.value.trim();
+        e.preventDefault();
+        if (url) {
+            Links.insert({ url });
+            this.refs.url.value = '';
+        }
     }
     render() {
         return (
             <div>
                 <h1>Your Links</h1>
                 <button onClick={this.onLogout.bind(this)}>Logout</button>
+                <LinksList/>
+                <p>Add Link</p>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" ref="url" placeholder="url"/>
+                    <button>Add Link</button>
+                </form>
             </div>
         );
     }
--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/users'; // this will execute the file - has account.validate
+import '../imports/api/links';
 
 Meteor.startup(() => {
   // code to run on server at startup
