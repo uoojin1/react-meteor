44aed1345df188d3d86487c8a1fa1a1f62832ae3
uoojin1 <uoojin@hotmail.com>
2017-11-17 01:13:47 -0500

    moved LinksListItem to its own component

---
+++ short-lnk/imports/ui/PrivateHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Accounts } from 'meteor/accounts-base';
+
+const PrivateHeader = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <button onClick={() => Accounts.logout()}>Logout</button>
+        </div>
+    );
+}
+
+PrivateHeader.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
+
+export default PrivateHeader;
